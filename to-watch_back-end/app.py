ab4a79c104a590127fcb0187d3ade9de7ccb6070
LRiosB <lucas.riosb@gmail.com>
2024-12-07 17:04:11 -0300

    setting flask dummies

---
+++ to-watch_back-end/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/search/<query_term>")
+def search(query_term):
+    return f"<p>Searching for \"{query_term}\" </p>"
+
+@app.route("/movie/<movie_id>")
+def getMovieInfo(movie_id):
+    return f"<p>Getting movie info for \"{movie_id}\" </p>"
+
+@app.route("/reorder/<list_id>/<movie_id>/<operation>", methods=["POST"])
+def reorderElement(list_id, movie_id, operation):
+
+    if operation not in ["up", "down", "first", "last"]:
+        return f"<p>Invalid operation \"{operation}\" </p>"
+
+    return f"<p> Reordering, on list \"{list_id}\", the movie \"{movie_id}\" with the operation \"{operation}\" </p>"
+
+@app.route("/getOrder/<list_id>")
+def getListOrder(list_id):
+    return f"<p>Getting list order for \"{list_id}\" </p>"
+
+@app.route("/setOrder/<list_id>", methods=["POST"])
+def setListOrder(list_id):
+
+    data = request.get_json()
+
+    return f"<p>Setting list order for \"{list_id}\", with json = {data} </p>"
