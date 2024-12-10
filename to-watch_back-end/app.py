df5f80cdf8d106424cb9a7ca11d2c5da6b96f8e6
LRiosB <lucas.riosb@gmail.com>
2024-12-10 18:39:10 -0300

    main program changes

--- to-watch_back-end/app.py
+++ to-watch_back-end/app.py
@@ -3,32 +3,72 @@ from mainProgram import mainProgram
 
 app = Flask(__name__)
 
-@app.route("/imdbmultisearch/<query_term>")
+@app.route("/imdbMultiSearch/<query_term>")
 def multisearch(query_term):
     return mainProgram().imdbSearchMultipleMovies(query_term)
 
-@app.route("/imdbsinglesearch/<imdbID>")
+@app.route("/imdbSingleSearch/<imdbID>")
 def singlesearch(imdbID:str):
     return mainProgram().imdbSearchSingleMovie(str(imdbID))
 
-@app.route("/movie/<movie_id>")
+@app.route("/getMovieInfo/<movie_id>")
 def getMovieInfo(movie_id):
-    return f"<p>Getting movie info for \"{movie_id}\" </p>"
+    return mainProgram().getMovieInfo(int(movie_id))
 
-@app.route("/reorder/<list_id>/<movie_id>/<operation>", methods=["POST"])
-def reorderElement(list_id, movie_id, operation):
-    if operation not in ["up", "down", "first", "last"]:
-        abort(400) # Bad request
-    return f"<p> Reordering, on list \"{list_id}\", the movie \"{movie_id}\" with the operation \"{operation}\" </p>"
+@app.route("/createNewMovie/<title>", methods=["POST"])
+def createNewMovie(title:str):
+    year = request.args.get("year", default=None, type=int)
+    kind = request.args.get("kind", default=None, type=str)
+    cover_url = request.args.get("cover_url", default=None, type=str)
+    imdb_id = request.args.get("imdb_id", default=None, type=str)
+    rating = request.args.get("rating", default=None, type=float)
+    return mainProgram().createNewMovie(title=title, year=year, kind=kind, cover_url=cover_url, imdb_id=imdb_id, rating=rating)
 
-@app.route("/getOrder/<list_id>")
-def getListOrder(list_id):
-    return f"<p>Getting list order for \"{list_id}\" </p>"
+@app.route("/editMovieInfo/<movie_id>", methods=["POST"])
+def editMovieInfo(movie_id):
+    title = request.args.get("title", default=None, type=str)
+    year = request.args.get("year", default=None, type=int)
+    kind = request.args.get("kind", default=None, type=str)
+    cover_url = request.args.get("cover_url", default=None, type=str)
+    imdb_id = request.args.get("imdb_id", default=None, type=str)
+    rating = request.args.get("rating", default=None, type=float)
+    return mainProgram().editMovieInfo(int(movie_id), title=title, year=year, kind=kind, cover_url=cover_url, imdb_id=imdb_id, rating=rating)
 
-@app.route("/setOrder/<list_id>", methods=["POST"])
-def setListOrder(list_id):
-    data = request.get_json()
-    return f"<p>Setting list order for \"{list_id}\", with json = {data} </p>"
+@app.route("/getListInfo/<list_id>")
+def getListInfo(list_id):
+    return mainProgram().getListInfo(int(list_id))
+
+@app.route("/createNewList/<list_name>", methods=["POST"])
+def createNewList(list_name):
+    return mainProgram().createNewList(list_name)
+
+@app.route("/putMovieOnList/<movie_id>/<list_id>", methods=["POST"])
+def putMovieOnList(movie_id, list_id):
+    return mainProgram().putMovieOnList(int(movie_id), int(list_id))
+
+@app.route("/setNewOrderOnList/<list_id>", methods=["POST"])
+def setNewOrderOnList(list_id):
+    raise NotImplementedError
+
+
+
+
+
+
+# @app.route("/reorder/<list_id>/<movie_id>/<operation>", methods=["POST"])
+# def reorderElement(list_id, movie_id, operation):
+#     if operation not in ["up", "down", "first", "last"]:
+#         abort(400) # Bad request
+#     return f"<p> Reordering, on list \"{list_id}\", the movie \"{movie_id}\" with the operation \"{operation}\" </p>"
+
+# @app.route("/getOrder/<list_id>")
+# def getListOrder(list_id):
+#     return f"<p>Getting list order for \"{list_id}\" </p>"
+
+# @app.route("/setOrder/<list_id>", methods=["POST"])
+# def setListOrder(list_id):
+#     data = request.get_json()
+#     return f"<p>Setting list order for \"{list_id}\", with json = {data} </p>"
 
 
 if __name__ == '__main__':
--- to-watch_back-end/mainProgram.py
+++ to-watch_back-end/mainProgram.py
@@ -2,6 +2,9 @@ from imdbSearch import imdbSearcher
 from database import dbInterface, DatabaseException
 from databaseSQLAlchemy import dbInterfaceSQLAlchemy
 
+class MainProgramException(Exception):
+    pass
+
 class mainProgram:
     def __init__(self, engine="sqlite:///towatch.db"):
         self.imdbSearcher:imdbSearcher = imdbSearcher()
@@ -37,3 +40,112 @@ class mainProgram:
         - plot outline
         """
         return self.imdbSearcher.imdbSearchSingleMovie(imdbID)
+    
+    def getMovieInfo(self, movie_id:int):
+        try:
+            return self.db.getMovieInfo(movie_id=movie_id)
+        except DatabaseException as e:
+            raise MainProgramException(str(e))
+
+    def createNewMovie(self, title:str, year:int=None, kind:str=None, cover_url:str=None, imdb_id:str=None, rating:float=None):
+        return self.db.createNewMovie(title=title, year=year, kind=kind, cover_url=cover_url, imdb_id=imdb_id, rating=rating)
+
+    def editMovieInfo(self, movie_id:int, title:str=None, year:int=None, kind:str=None, cover_url:str=None, imdb_id:str=None, rating:float=None):
+        if movie_id not in self.db.getAllMoviesIds():
+            raise MainProgramException("Movie id not found")
+
+        if title is not None:
+            self.db.setMovieTitle(movie_id=movie_id, title=title)
+        if year is not None:
+            self.db.setMovieYear(movie_id=movie_id, year=year)
+        if kind is not None:
+            self.db.setMovieKind(movie_id=movie_id, kind=kind)
+        if cover_url is not None:
+            self.db.setMovieCoverUrl(movie_id=movie_id, cover_url=cover_url)
+        if imdb_id is not None:
+            self.db.setMovieImdbID(movie_id=movie_id, imdb_id=imdb_id)
+        if rating is not None:
+            self.db.setMovieRating(movie_id=movie_id, rating=rating)
+
+    def getListInfo(self, list_id:int):
+        try:
+            name = self.db.getListName(list_id=list_id)
+            order = self.db.getListOrder(list_id=list_id)
+            return {"name":name, "order":order}
+        except DatabaseException as e:
+            raise MainProgramException(str(e))
+
+    def createNewList(self, list_name:str):
+        return self.db.createNewList(list_name=list_name)
+
+    def putMovieOnList(self, movie_id:int, list_id:int):
+
+        if list_id not in self.db.getAllListsIds():
+            raise MainProgramException("List id not found")
+        if movie_id not in self.db.getAllMoviesIds():
+            raise MainProgramException("Movie id not found")
+
+        order = self.db.getListOrder(list_id=list_id)
+
+        if movie_id in order:
+            raise MainProgramException("Movie already in list")
+
+        self.db.setListOrder(list_id=list_id, order=newOrder)
+        newOrder = order.copy()
+        newOrder.append(movie_id)
+
+    def setNewOrderOnList(self, list_id:int, newOrder:list[int]):
+        if list_id not in self.db.getAllListsIds():
+            raise MainProgramException("List id not found")
+        
+        oldOrder = self.db.getListOrder(list_id=list_id)
+
+        dictOld = dict()
+        dictNew = dict()
+
+        for indice in oldOrder:
+            dictNew[indice] += 1
+        for indice in newOrder:
+            dictNew[indice] += 1
+        
+        if dictOld != dictNew:
+            raise MainProgramException("New order is not valid")
+
+        self.db.setListOrder(list_id=list_id, order=newOrder)
+
+    def reorderSingleMovieOnList(self, movie_id:int, operation:str, list_id:int):
+        if operation not in ["up", "down", "first", "last"]:
+            raise MainProgramException("Invalid operation")
+
+        currentOrder:list[int] = self.db.getListOrder(list_id)
+        
+        position = -1
+        for i in range(len(currentOrder)):
+            if currentOrder[i] == movie_id:
+                position = i
+                break
+        
+        if position == -1:
+            raise MainProgramException("Movie not found on list")
+
+        if operation == "up" and position == 0:
+            raise MainProgramException("Movie is already at the top of the list")
+        if operation == "down" and position == len(currentOrder) - 1:
+            raise MainProgramException("Movie is already at the bottom of the list")
+        
+        newOrder = currentOrder.copy()
+        if operation == "up":
+            newOrder[position-1], newOrder[position] = newOrder[position], newOrder[position-1]
+        elif operation == "down":
+            newOrder[position+1], newOrder[position] = newOrder[position], newOrder[position+1]
+        elif operation == "first":
+            newOrder.pop(position)
+            newOrder.insert(0, movie_id)
+        elif operation == "last":
+            newOrder.pop(position)
+            newOrder.append(movie_id)
+
+        self.db.setListOrder(list_id, newOrder)
+
+    
+
