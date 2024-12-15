075bbd3ca5845115fd94d2eaf1e2e910477e15f9
Isabel Elise <80064876+isabel-elise@users.noreply.github.com>
2024-12-15 13:04:32 -0300

    Add support for saving list

--- to-watch_front-end/src/sections/MovieLineListSection/MovieLineListSection.tsx
+++ to-watch_front-end/src/sections/MovieLineListSection/MovieLineListSection.tsx
@@ -3,22 +3,39 @@ import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { MovieList } from "../../interfaces";
 
 import "./movieLineListSection.css";
+import { RiSave3Line } from "react-icons/ri";
 
 export interface MovieLineListSectionProps {
   currentList: MovieList;
   avaiableLists: { id: number; name: string }[];
   onSelectList: (id: number) => void;
+  onSaveList: (id: number, newOrder: number[]) => void;
 }
 
 export function MovieLineListSection({
   currentList,
   avaiableLists,
   onSelectList,
+  onSaveList,
 }: MovieLineListSectionProps) {
   const [parent] = useAutoAnimate();
   return (
     <section className="movie-line-list-section">
-      <header className="movie-line-list-header">{currentList.name}</header>
+      <header className="movie-line-list-header">
+        <div>{currentList.name}</div>
+        <button
+          id="save-list"
+          onClick={() => {
+            alert("Salvando lista: " + currentList.name);
+            onSaveList(
+              currentList.id,
+              currentList.entries.map((entry) => entry.id)
+            );
+          }}
+        >
+          <RiSave3Line size="1.4em" />
+        </button>
+      </header>
       <section className="movie-line-list" ref={parent}>
         {currentList.entries.map((movie) => {
           return (
