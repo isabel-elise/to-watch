323d24a1c6c6fdb3c89fd4f0aec4c5039cdd2028
LRiosB <lucas.riosb@gmail.com>
2024-12-15 16:35:59 -0300

    endpoint implementation

--- to-watch_back-end/app.py
+++ to-watch_back-end/app.py
@@ -3,6 +3,13 @@ from mainProgram import mainProgram, MainProgramException
 
 app = Flask(__name__)
 
+
+@app.route("/test")
+def test():
+    aux = mainProgram().getListCompleteMovieData(1)
+    return str(aux)
+
+
 # Recuperação inicial de dados
 
 # Listas disponíveis
@@ -12,8 +19,8 @@ def getEveryListInfo():
 
 # Entradas na lista
 @app.route("/movies/<list_id>")
-def getMovieInfo(list_id:int):
-    return mainProgram().getListCompleteMovieData(list_id=list_id)
+def getMovieInfo(list_id):
+    return mainProgram().getListCompleteMovieData(list_id=int(list_id))
 
 
 # Buscar dados do imdb
@@ -34,20 +41,84 @@ def getMovieIMDB(imdb_id:str):
 # Adicionar entrada na lista
 @app.route("/add_movie/<list_id>", methods=["POST"])
 def addMovieToList(list_id:int):
-    raise NotImplementedError
+    data = request.get_json()
+
+    list_id = int(list_id)
+
+    if data is None:
+        abort(400, "Missing data")
+
+    if "title" in data:
+        title = data["title"]
+    else:
+        abort(400, "Missing title")
+
+    year = None
+    if "year" in data:
+        year = int(data["year"])
+    
+    kind = None
+    if "kind" in data:
+        kind = data["kind"]
+
+    cover_url = None
+    if "cover_url" in data:
+        cover_url = data["cover_url"]
+
+    imdb_id = None
+    if "imdb_id" in data:
+        imdb_id = data["imdb_id"]
+
+    rating = None
+    if "rating" in data:
+        rating = float(data["rating"])
+
+    try:
+        movie_id = mainProgram().createNewMovie(title=title, year=year, kind=kind, cover_url=cover_url, imdb_id=imdb_id, rating=rating)
+        mainProgram().putMovieOnList(movie_id=movie_id, list_id=list_id)
+    except MainProgramException as e:
+        abort(400, str(e))
+
+    return mainProgram().getListCompleteMovieData(list_id=int(list_id))
 
 # Adicionar lista
-@app.route("/add_list/", methods=["POST"])
+@app.route("/add_list", methods=["POST"])
 def addNewList():
-    raise NotImplementedError
+    data = request.get_json()
+
+    if data is None:
+        abort(400, "Missing data")
+    if "list_name" not in data:
+        abort(400, "Missing list name")
+
+    list_name = data["list_name"]
+    list_id = mainProgram().createNewList(list_name)
+    
+    return mainProgram().getAllListsIdsAndNamesInDatabase()
 
 
 # Salvar
 
-# Salvar lista
+# Salvar lista (salvar nova ordem da lista)
 @app.route("/save_list/<list_id>", methods=["POST"])
 def saveList(list_id:int):
-    raise NotImplementedError
+    data = request.get_json()
+
+    list_id = int(list_id)
+
+    if data is None:
+        abort(400, "Missing data")
+    if "order" not in data:
+        abort(400, "Missing order")
+
+    order = data["order"]
+    
+    try:
+        mainProgram().setNewOrderOnList(list_id=list_id, newOrder=order)
+    except MainProgramException as e:
+        abort(400, str(e))
+
+    return mainProgram().getListCompleteMovieData(list_id=list_id)
 
 
 
